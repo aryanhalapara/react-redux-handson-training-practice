3b6cc6f4aa787309636c626a24a1978f6b24e2b6
“aryan.halapara” <aryan.halapara>
2020-04-20 14:18:16 +0530

    added build

---
+++ Assignment-3/project-app/build/precache-manifest.cd2a6570a07dac78255a33edfb04c3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25238c6944ebc7cd48d2d855b90b0912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bd7366f1a98583e5bd9",
+    "url": "/static/css/main.dbf86ba6.chunk.css"
+  },
+  {
+    "revision": "c74f8c4e4ebfb2d1b35b",
+    "url": "/static/js/2.9d148d35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d148d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd7366f1a98583e5bd9",
+    "url": "/static/js/main.49fe37b9.chunk.js"
+  },
+  {
+    "revision": "76f486685270ecd987ea",
+    "url": "/static/js/runtime-main.88389f74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
